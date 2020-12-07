2478fb821efa3a230d82407ab9c80b53891f2f5c
Jason Zions <jasonzio@microsoft.com>
2020-12-07 11:15:29 -0800

    Refactor

--- 07/day07.py
+++ 07/day07.py
@@ -1,32 +1,31 @@
 from collections import defaultdict
-from typing import Tuple, List, Dict
+from typing import Tuple, List, Dict, Set
 import re
 
 
 bag_spec_re = re.compile(r'(^\d+) (.*)$')
 
 
-def cleanup_color(color: str) -> str:
-    if color[-1] == '.':
-        color = color[:-1]
-    if color[-1] == 's':
-        color = color[:-1]
-    return color
-
-
 def parse_rule(rule: str) -> Tuple[str, Dict[str, int]]:
+    def normalize_color(color: str) -> str:
+        if color[-1] == '.':
+            color = color[:-1]
+        if color[-1] == 's':
+            color = color[:-1]
+        return color
+
     outer, inner_set = rule.split(" contain ")
-    outer = cleanup_color(outer)
+    outer = normalize_color(outer)
     inner = {}
     for inner_spec in inner_set.split(", "):
         match = bag_spec_re.match(inner_spec)
         if match:
-            inner[cleanup_color(match.group(2))] = int(match.group(1))
+            inner[normalize_color(match.group(2))] = int(match.group(1))
 
     return outer, inner
 
 
-def close_contents(color: str, contained_by: Dict[str, List[str]]) -> List[str]:
+def close_contents(color: str, contained_by: Dict[str, List[str]]) -> Set[str]:
     search_list = contained_by[color]
     already_searched = set()
     results = set()
@@ -43,18 +42,13 @@ def count_contained_bags(color: str, contains: Dict[str, Dict[str, int]]) -> int
 
 
 with open('input.txt', 'r') as f:
-    rules = [line.strip() for line in f.readlines()]
+    parsed_rules = [parse_rule(line.strip()) for line in f.readlines()]
 
+contains = {outer: inner for outer, inner in parsed_rules}
 contained_by = defaultdict(list)
-contains = {}
-for rule in rules:
-    outer, inner = parse_rule(rule)
-    contains[outer] = inner
-    for inner_color in inner.keys():
-        contained_by[inner_color].append(outer)
+for outer, inner in contains.items():
+    {contained_by[ic].append(outer) for ic in inner}
 
 sgb = "shiny gold bag"
-results = close_contents(sgb, contained_by)
-print('Bag colors holding {}s: {}'.format(sgb, len(results)))
-
+print('Bag colors holding {}s: {}'.format(sgb, len(close_contents(sgb, contained_by))))
 print('Bags inside the {}: {}'.format(sgb, count_contained_bags(sgb, contains)))
